e327b0336efaf473862f213050e2487908c62804
Ankit Sultana <ankitsultana@gmail.com>
2025-11-22 01:08:14 -0600

    add some buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu"
-import { ChevronDown } from "lucide-react";
+import { ArrowDownAZ, ChartArea, ChevronDown, SortAsc } from "lucide-react";
 import { LayoutGrid } from "lucide-react"; // or Squares2X2 from Heroicons
 import { JSX } from "react";
 
@@ -21,17 +21,69 @@ function createButton(text: string, elem: JSX.Element) {
   )
 }
 
+function createBlueProminentButton(text: string) {
+  return (
+    <Button variant="outline" className="max-w-min px-2! py-0! gap-0! bg-[rgb(38,110,240)] text-white hover:bg-[rgb(30,90,220)] hover:text-white">
+      <ChartArea />
+      <span className="text-sm font-semibold text-white">{ text }</span>
+    </Button>
+  )
+}
+
+function createDropdownFixedDynamicButton(fixedText: string, dynamicText: string) {
+  return (
+    <Button variant="outline" className="bg-[rgb(251,251,251)] max-w-min px-2! py-2 gap-1">
+      <ArrowDownAZ />
+      <span className="text-sm font-light">{ fixedText }</span>
+      <span className="text-sm font-semibold">{ dynamicText }</span>
+      <ChevronDown stroke="#888" />
+    </Button>
+  )
+}
+
 export default function Home() {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="max-w-min" asChild>
-        { createButton("All Companies", greenBoard) }
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-      <DropdownMenuItem>
-        Something
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu key="companies">
+        <DropdownMenuTrigger className="max-w-min" asChild>
+          { createButton("All Companies", greenBoard) }
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>
+            Something
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DropdownMenu key="add-report-1">
+        <DropdownMenuTrigger className="max-w-min" asChild>
+          { createBlueProminentButton("Add report") }
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>
+            Something
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DropdownMenu key="add-report-2">
+        <DropdownMenuTrigger className="max-w-min" asChild>
+          { createBlueProminentButton("  Add report") }
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>
+            Something
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DropdownMenu key="add-report-3">
+        <DropdownMenuTrigger className="max-w-min" asChild>
+          { createDropdownFixedDynamicButton("Sorted by", "order_timestamp_utc") }
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>
+            Something
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 }
